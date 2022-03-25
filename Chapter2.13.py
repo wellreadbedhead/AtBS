f8bb5b0d1c9eaccead73b9cc66a42eb9be6859c3
Andrew <andrewboudon93@gmail.com>
2022-03-24 20:54:02 -0400

    adding Chapter 2 exercise 13

---
+++ Chapter2.13.py
@@ -0,0 +1,13 @@
+def countToTen():
+    for i in range(1,11):
+        print(i)
+
+countToTen()
+
+def countUsingWhile():
+    a = 0
+    while a < 10:
+        a += 1
+        print(a)
+
+countUsingWhile()
