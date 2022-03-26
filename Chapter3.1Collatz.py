01a93d0cd7d719119482354dd638aa3257bfd271
Andrew <andrewboudon93@gmail.com>
2022-03-26 07:26:47 -0400

    adding Chapter 3 Collatz

---
+++ Chapter3.1Collatz.py
@@ -0,0 +1,18 @@
+def collatz(a):
+    if a%2 == 0:
+        a //= 2
+        print(str(a))
+        return(a)
+    elif a%2 == 1:
+        a = a * 3 + 1
+        print(str(a))
+        return(a)
+    else:
+        print("Please only enter integers")
+
+def getToOne():
+    a = int(input("Enter an integer: "))
+    while a != 1:
+        a = collatz(a)
+
+getToOne()
